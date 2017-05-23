a352fa6980d61ef210f930805dcc9ee5e22b17c7
Chris <orthagonal1@gmail.com>
2017-05-23 15:02:41 -0500

    overload classModule()

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 'use strict';
 const TaskKitTask = require('taskkit-task');
 const fs = require('fs-extra');
-
+const path = require('path');
 class CleanTask extends TaskKitTask {
   get description() {
     return 'Cleans out the indicated directory';
+  }
+  // returns the module to load when running in a separate process:
+  get classModule() {
+    return path.join(__dirname, 'index.js');
   }
 
   process(input, filename, processDone) {
